36a62deb0cdcba287b89e1c26d785a70887cf0ec
Shobhit Rawat <sbtrwt.se@gmail.com>
2023-10-28 14:44:10 +0530

    Snake script added

---
+++ Assets/scripts/GameAssets.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    public static GameAssets Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public Sprite snakeHeadSprite;
+
+
+
+}
